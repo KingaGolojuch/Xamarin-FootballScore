0b70a3fa5a614d09612de327d6196099582da25e
Unknown <wywpawel@gmail.com>
2018-06-26 23:53:33 +0200

    Removing unnecessary items.

--- PROJEKT/PROJEKT/PROJEKT/App.xaml.cs
+++ PROJEKT/PROJEKT/PROJEKT/App.xaml.cs
@@ -13,22 +13,7 @@ namespace PROJEKT
         {
             InitializeComponent();
 
-            MainPage = new PROJEKT.MainPage();
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
+            //MainPage = new PROJEKT.MainPage();
         }
     }
 }
