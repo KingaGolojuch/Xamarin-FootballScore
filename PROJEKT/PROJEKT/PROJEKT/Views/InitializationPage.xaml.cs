b8f82d7d760623b42f6bd843bb5793927790c986
Unknown <kinga.golojuch@gmail.com>
2018-06-27 18:23:01 +0200

    Adding main page - InitializationPage.xaml

---
+++ PROJEKT/PROJEKT/PROJEKT/Views/InitializationPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PROJEKT.Views
+{
+    public partial class InitializationPage : ContentPage
+    {
+        public InitializationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
