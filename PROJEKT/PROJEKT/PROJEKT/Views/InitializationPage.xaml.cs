25c2ef197f2c1dfd8f7ba32b477e75d4ae3c9f99
Unknown <rafal.wywrot@onet.pl>
2018-06-27 18:31:16 +0200

    Connecting the View to the ViewModel. Adding navigation to next page

--- PROJEKT/PROJEKT/PROJEKT/Views/InitializationPage.xaml.cs
+++ PROJEKT/PROJEKT/PROJEKT/Views/InitializationPage.xaml.cs
@@ -10,9 +10,27 @@ namespace PROJEKT.Views
 {
     public partial class InitializationPage : ContentPage
     {
+        InitializationViewModel vm;
+
         public InitializationPage()
         {
             InitializeComponent();
+            vm = new InitializationViewModel();
+            BindingContext = vm;
+            SizeChanged += (s, e) => vm.SizeChanged(Width, Height);
+        }
+
+        protected override async void OnAppearing()
+        {
+            await Task.Delay(1000);
+            await Navigation.PushAsync(new CompetitionsPage());
+        }
+
+        private void Image_SizeChanged(object s, EventArgs e)
+        {
+            Image img = (Image)s;
+            img.HeightRequest = (Application.Current as App).Height;
+            img.WidthRequest = (Application.Current as App).Width;
         }
     }
 }
