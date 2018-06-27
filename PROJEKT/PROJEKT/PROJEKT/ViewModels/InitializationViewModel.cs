f8be3eaa3e8d0c22c218c67a16249c08b856fda2
Unknown <rafal.wywrot@onet.pl>
2018-06-27 18:29:55 +0200

    Adding class InitializationViewModel

---
+++ PROJEKT/PROJEKT/PROJEKT/ViewModels/InitializationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJEKT.ViewModels
+{
+    public class InitializationViewModel
+    {
+        public void SizeChanged(double width, double height)
+        {
+            (Application.Current as App).Width = width;
+            (Application.Current as App).Height = height;
+        }
+    }
+}
