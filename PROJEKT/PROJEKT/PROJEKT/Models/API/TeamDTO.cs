71de9205661b777500d5ac8a8a500ea5804438d9
Unknown <rafal.wywrot@onet.pl>
2018-06-27 19:29:58 +0200

    Adding auxiliary classes to process API data in the form of JSON

---
+++ PROJEKT/PROJEKT/PROJEKT/Models/API/TeamDTO.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace PROJEKT.Models.API
+{
+    /// <summary>
+    /// Klasa pomocnicza do wczytywania danych z API za pomocą JSON.
+    /// Dane pojedynczego zespołu.
+    /// </summary>
+    public class TeamDTO
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
