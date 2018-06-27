3a4601bae8689b403179b45300d3bda3f0b89c4c
Unknown <rafal.wywrot@onet.pl>
2018-06-27 19:27:44 +0200

    Adding items to ViewModels

---
+++ PROJEKT/PROJEKT/PROJEKT/ViewModels/CompetitionViewModel.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using PROJEKT.Models;
+using PROJEKT.Models.API;
+using PROJEKT.Views.Controls;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PROJEKT.ViewModels
+{
+    /// <summary>
+    /// Klasa pomocnicza do obsłużenia widoku CompetitionPage.
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class CompetitionViewModel
+    {
+        /// <summary>
+        /// Obiekt kontrolki MyCarouselView.
+        /// </summary>
+        public MyCarouselView view = null;
+        /// <summary>
+        /// Obiekty kontrolek w widoku.
+        /// </summary>
+        public Grid GHome = null;
+        public Grid GFixtures = null;
+        public Grid GLeague = null;
+        public Grid GTeams = null;
+        /// <summary>
+        /// Lista obiektów podstron.
+        /// </summary>
+        public List<CustomCompetition> ListPages { get; set; }
+        /// <summary>
+        /// Akcje do widoku.
+        /// </summary>
+        public Command CmdHome { get; set; }
+        public Command CmdFixtures { get; set; }
+        public Command CmdLeague { get; set; }
+        public Command CmdTeams { get; set; }
+
+        /// <summary>
+        /// Konstruktor.
+        /// Implementacja akcji do widoku.
+        /// </summary>
+        public CompetitionViewModel()
+        {
+            CmdHome = new Command(() =>
+            {
+                view.SelectedIndex = 0;
+                ChangeBackground(GHome);
+            });
+            CmdFixtures = new Command(() =>
+            {
+                view.SelectedIndex = 1;
+                ChangeBackground(GFixtures);
+            });
+            CmdLeague = new Command(() =>
+            {
+                view.SelectedIndex = 2;
+                ChangeBackground(GLeague);
+            });
+            CmdTeams = new Command(() =>
+            {
+                view.SelectedIndex = 3;
+                ChangeBackground(GTeams);
+            });
+        }
+
+        /// <summary>
+        /// Metoda do zmiany tła w menu.
+        /// </summary>
+        public void ChangeBackground(Grid grid)
+        {
+            GTeams.BackgroundColor = Color.FromHex("#073a0a");
+            GFixtures.BackgroundColor = Color.FromHex("#073a0a");
+            GLeague.BackgroundColor = Color.FromHex("#073a0a");
+            GHome.BackgroundColor = Color.FromHex("#073a0a");
+            grid.BackgroundColor = Color.FromHex("#efefef");
+        }
+
+        /// <summary>
+        /// Metoda stwarzająca podstrony.
+        /// </summary>
+        public void DownloadData()
+        {
+            ListPages = new List<CustomCompetition>()
+            {
+                new CustomCompetition() { Id = 1},
+                new CustomCompetition() { Id = 2},
+                new CustomCompetition() { Id = 3},
+                new CustomCompetition() { Id = 4}
+            };
+            view.ItemsSource = ListPages;
+        }
+    }
+}
---
+++ PROJEKT/PROJEKT/PROJEKT/ViewModels/CompetitionsViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using PROJEKT.Models;
+using PROJEKT.Models.API;
+using PROJEKT.Views.Controls;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace PROJEKT.ViewModels
+{
+    /// <summary>
+    /// Klasa pomocnicza do obsłużenia widoku CompetitionsPage.
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class CompetitionsViewModel
+    {
+        /// <summary>
+        /// Lista CustomCompetition, która jest źródłem do kontrolki ListView.
+        /// </summary>
+        public List<CustomCompetition> Competitions { get; set; }
+        /// <summary>
+        /// Lista obiektów CompetitionDTO do przekonwertowania danych z JSON.
+        /// </summary>
+        public List<CompetitionDTO> Results { get; set; }
+
+        /// <summary>
+        /// Metoda do pobrania z API danych.
+        /// </summary>
+        public async void DownloadData(ListView listView, Grid grid, Label lblNoInternet)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(new Uri(Configuration.API_COMPETITIONS));
+                string responseJson = await response.Content.ReadAsStringAsync();
+                Results = JsonConvert.DeserializeObject<List<CompetitionDTO>>(responseJson);
+            }
+            catch (Exception ex)
+            { }
+
+            Competitions = new List<CustomCompetition>();
+
+            if (Results != null)
+            {
+                DataView(listView, grid);
+            }
+            else
+            {
+                lblNoInternet.IsVisible = true;
+            }
+            
+        }
+
+        /// <summary>
+        /// Metoda wczytująca dane do widoku.
+        /// </summary>
+        private void DataView(ListView listView, Grid grid)
+        {
+            foreach (var result in Results)
+            {
+                CustomCompetition custom = new CustomCompetition()
+                {
+                    Id = result.Id,
+                    Caption = result.Caption,
+                    League = result.League,
+                    Year = result.Year,
+                    CurrentMatchday = result.CurrentMatchday.ToString(),
+                    NumberOfMatchdays = result.NumberOfMatchdays.ToString(),
+                    NumerOfTeams = result.NumberOfTeams.ToString()
+                };
+                Competitions.Add(custom);
+            }
+
+            listView.ItemsSource = Competitions;
+            grid.IsVisible = false;
+        }
+    }
+}
