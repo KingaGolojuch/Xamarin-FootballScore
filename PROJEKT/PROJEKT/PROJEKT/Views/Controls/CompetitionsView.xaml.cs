6cef9c0a0f10f5b52191588e2be2c007305c316c
Unknown <kinga.golojuch@gmail.com>
2018-06-27 19:21:52 +0200

    Adding CompetitionsView

---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/CompetitionsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    public partial class CompetitionsView : ViewCell
+    {
+        public CompetitionsView()
+        {
+            InitializeComponent();
+            View.BackgroundColor = Color.FromHex("#efefef");
+        }
+    }
+}
