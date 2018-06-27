ad19420044d0a337d77b782f50981535ac0b19c7
Unknown <kinga.golojuch@gmail.com>
2018-06-27 19:20:59 +0200

    Adding CompetitionsPage and CompetitionPage

---
+++ PROJEKT/PROJEKT/PROJEKT/Views/CompetitionPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using PROJEKT.ViewModels;
+using System;
+
+using Xamarin.Forms;
+
+namespace PROJEKT.Views
+{
+    public partial class CompetitionPage : ContentPage
+    {
+        private CompetitionViewModel _vm;
+
+        public CompetitionPage()
+        {
+            InitializeComponent();
+            _vm = new CompetitionViewModel()
+            {
+                view = crlLayout,
+                GHome = homeGrid,
+                GFixtures = fixturesGrid,
+                GLeague = leagueGrid,
+                GTeams = teamGrid
+            };
+            BindingContext = _vm;
+        }
+
+        protected override void OnAppearing()
+        {
+            crlLayout.WidthRequest = (Application.Current as App).Width;
+            footer.HeightRequest = (Application.Current as App).Width / 6;
+            _vm.DownloadData();
+            crlLayout.page = this;
+        }
+
+        public void ChangeSelectedIndex(int target)
+        {
+            switch (target)
+            {
+                case 0:
+                    _vm.ChangeBackground(homeGrid);
+                    break;
+                case 1:
+                    _vm.ChangeBackground(fixturesGrid);
+                    break;
+                case 2:
+                    _vm.ChangeBackground(leagueGrid);
+                    break;
+                case 3:
+                    _vm.ChangeBackground(teamGrid);
+                    break;
+                default:
+                    _vm.ChangeBackground(homeGrid);
+                    break;
+
+            }
+        }
+
+        private void Image_SizeChanged(object sender, EventArgs e)
+        {
+            Image image = (sender as Image);
+            image.WidthRequest = (Application.Current as App).Width / 7;
+            image.HeightRequest = (Application.Current as App).Width / 7;
+        }
+    }
+}
---
+++ PROJEKT/PROJEKT/PROJEKT/Views/CompetitionsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PROJEKT.Models;
+using PROJEKT.ViewModels;
+using PROJEKT.Views.Controls;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PROJEKT.Views
+{
+    public partial class CompetitionsPage : ContentPage
+    {
+        private CompetitionsViewModel _vm;
+
+        public CompetitionsPage()
+        {
+            InitializeComponent();
+            _vm = new CompetitionsViewModel();
+            BindingContext = _vm;
+            Title = "ZAWODY";
+            grid.IsVisible = true;
+            LblNoInternet.IsVisible = false;
+        }
+
+        protected override void OnAppearing()
+        {
+            listView.ItemTemplate = new DataTemplate(typeof(CompetitionsView));
+            _vm.DownloadData(listView, grid, LblNoInternet);
+
+            listView.ItemSelected += async (s, e) =>
+            {
+                grid.IsVisible = true;
+                CustomCompetition item = (CustomCompetition)e.SelectedItem;
+                CompetitionPage page = new CompetitionPage();
+                page.Title = item.Caption;
+                (Application.Current as App).CompetitionId = item.Id;
+                //await Task.Delay(1000);
+                await Navigation.PushAsync(page);
+            };
+        }
+    }
+}
