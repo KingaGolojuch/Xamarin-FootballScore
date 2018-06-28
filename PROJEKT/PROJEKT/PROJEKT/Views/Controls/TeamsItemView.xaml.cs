c0c9e851ab77099d112a906078db8c9fa5f9250f
Unknown <kinga.golojuch@gmail.com>
2018-06-28 18:30:03 +0200

    Adding TeamsView and TemasItemView

---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/TeamsItemView.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    public partial class TeamsItemView : ViewCell
+    {
+        public TeamsItemView()
+        {
+            InitializeComponent();
+            View.BackgroundColor = Color.FromHex("#efefef");
+        }
+    }
+}
---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/TeamsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PROJEKT.Models;
+using PROJEKT.Models.Extensions;
+using PROJEKT.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    public partial class TeamsView : ContentView
+    {
+        private TeamItemViewModel _vm;
+
+        public TeamsView()
+        {
+            InitializeComponent();
+            this.WidthRequest = (Application.Current as App).Width;
+            _vm = new TeamItemViewModel();
+
+            _vm.DownloadData(listView);
+        }
+    }
+}
