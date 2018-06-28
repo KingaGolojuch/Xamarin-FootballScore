a898110cc8a808eb710d5ee4e5e0c34debe17e25
PawelWywrot <wywpawel@gmail.com>
2018-06-28 22:02:01 +0200

    add interface downloadcompetition

---
+++ PROJEKT/PROJEKT/PROJEKT/Models/Interfaces/IDownloadCompetition.cs
@@ -0,0 +1,15 @@
+﻿using PROJEKT.Models.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJEKT.Models.Interfaces
+{
+    public interface IDownloadCompetition
+    {
+        System.Threading.Tasks.Task HttpCall();
+        List<CompetitionDTO> results { get; set; }
+    }
+}
