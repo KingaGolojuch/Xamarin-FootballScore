f0ded2f4ffecb6e2d09f8873dcdfd99c1b131f31
Unknown <kinga.golojuch@gmail.com>
2018-06-28 18:29:38 +0200

    Adding ComeptitionView

---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/CompetitionView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PROJEKT.ViewModels;
+using PropertyChanged;
+
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    [AddINotifyPropertyChangedInterface]
+    public partial class CompetitionView : ContentView
+    {
+        private CompetitionItemViewModel _vm;
+
+        public CompetitionView()
+        {
+            InitializeComponent();
+            this.WidthRequest = (Application.Current as App).Width;
+            _vm = new CompetitionItemViewModel();
+
+            _vm.DownloadData(caption, year, currentMatchday, numberOfMatchdays, numberOfTeams);
+            
+        }
+    }
+}
