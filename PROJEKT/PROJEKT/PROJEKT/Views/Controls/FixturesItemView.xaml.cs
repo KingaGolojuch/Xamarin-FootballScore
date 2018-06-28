b5276757e99513b309fd38c7609a1e6570f0300c
Unknown <kinga.golojuch@gmail.com>
2018-06-28 18:30:37 +0200

    Adding FixturesView and FixturesItemView

---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/FixturesItemView.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    public partial class FixturesItemView : ViewCell
+    {
+        public FixturesItemView()
+        {
+            InitializeComponent();
+            View.BackgroundColor = Color.FromHex("#efefef");
+        }
+    }
+}
---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/FixturesView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PROJEKT.ViewModels;
+
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    public partial class FixturesView : ContentView
+    {
+        private FixtureItemViewModel _vm;
+
+        public FixturesView()
+        {
+            InitializeComponent();
+            this.WidthRequest = (Application.Current as App).Width;
+            _vm = new FixtureItemViewModel();
+
+            _vm.DownloadData(listView);
+        }
+    }
+}
