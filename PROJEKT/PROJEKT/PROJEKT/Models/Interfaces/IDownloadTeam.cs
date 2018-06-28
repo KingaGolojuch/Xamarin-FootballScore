03a3bbcce778e8ef578e2ff80e137f6488b54f72
PawelWywrot <wywpawel@gmail.com>
2018-06-28 22:57:07 +0200

    Adding DownloadTeams implementation for testing

--- PROJEKT/PROJEKT/PROJEKT/Models/Interfaces/IDownloadTeam.cs
+++ PROJEKT/PROJEKT/PROJEKT/Models/Interfaces/IDownloadTeam.cs
@@ -7,8 +7,12 @@ using System.Threading.Tasks;
 
 namespace PROJEKT.Models.Interfaces
 {
+    /// <summary>
+    /// Interfejs do pobrania danych dla Team
+    /// </summary>
     public interface IDownloadTeam
     {
+           
         System.Threading.Tasks.Task HttpCall();
         TeamsDTO results { get; set; }
 
