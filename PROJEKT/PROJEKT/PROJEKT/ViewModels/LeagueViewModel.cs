26d1cd0cbf7045e22be8bf956a733655a89fb9a1
Unknown <rafal.wywrot@onet.pl>
2018-06-28 18:35:00 +0200

    Adding LeagueViewModel

---
+++ PROJEKT/PROJEKT/PROJEKT/ViewModels/LeagueViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PropertyChanged;
+
+namespace PROJEKT.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class LeagueViewModel
+    {
+        public string Position { get; set; }
+        public string TeamName { get; set; }
+        public string PlayedGames { get; set; }
+        public string Points { get; set; }
+        public string Goals { get; set; }
+        public string Wins { get; set; }
+        public string Draws { get; set; }
+        public string Losses { get; set; }
+    }
+}
