dcecef3d2361fb9268ca4699cee82df9c9b54639
Unknown <kinga.golojuch@gmail.com>
2018-06-28 18:31:00 +0200

    Adding LeagueView and LeagueItemView

---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/LeagueItemView.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    public partial class LeagueItemView : ContentView
+    {
+        public LeagueItemView()
+        {
+            InitializeComponent();
+            this.WidthRequest = (Application.Current as App).Width;
+        }
+    }
+}
---
+++ PROJEKT/PROJEKT/PROJEKT/Views/Controls/LeagueView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PROJEKT.ViewModels;
+
+using Xamarin.Forms;
+
+namespace PROJEKT.Views.Controls
+{
+    public partial class LeagueView : ContentView
+    {
+        private LeagueItemViewModel _vm;
+
+        public LeagueView()
+        {
+            InitializeComponent();
+            this.WidthRequest = (Application.Current as App).Width;
+            _vm = new LeagueItemViewModel();
+
+            _vm.DownloadData(StlItem);
+        }
+    }
+}
